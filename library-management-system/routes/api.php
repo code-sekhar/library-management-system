d53d5ff6820b316624e4f554c232cf5e671b8e01
sekhar <sekharchaudhary5@gmail.com>
2024-07-12 02:11:34 +0530

    login register add

--- library-management-system/routes/api.php
+++ library-management-system/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+
+route::post('/register', [AuthController::class, 'register']);
+route::post('', [AuthController::class, 'login']);
+route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+//route::middleware('auth:api')->post('/logout', [AuthController::class, 'logout']);
--- library-management-system/routes/web.php
+++ library-management-system/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['web', 'auth'])->group(function () {
+    route::post('/register', [AuthController::class, 'register']);
+    route::post('', [AuthController::class, 'index']);
 });
